1cbbab2ca6a462da6d1a1eb02f6d74be24dccecc
Angel Herrera <angelherrerax7@gmail.com>
2017-11-21 18:27:17 -0500

    Creación del Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  get 'login', to: 'login#index'
+  root 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
