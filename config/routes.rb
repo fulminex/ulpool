c84f4400292dab3b8567d6ae5dd70d7dfdc873f0
Angel Herrera <angelherrerax7@gmail.com>
2017-11-23 16:40:10 -0500

    Agregado el /about

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :travels
   resources :users
   get 'login', to: 'login#index'
+  get 'about', to: 'about#index'
   root 'login#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
